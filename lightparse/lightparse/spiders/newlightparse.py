5b581c0408c6e1f7d78c583daedf5e5e56ac7bff
Вадим Голов <grafit-design@inbox.ru>
2024-10-26 01:52:42 +0500

    corrected spyder pattern

--- lightparse/lightparse/spiders/newlightparse.py
+++ lightparse/lightparse/spiders/newlightparse.py
@@ -1,9 +1,10 @@
 import scrapy
 
+
 class NewlightparseSpider(scrapy.Spider):
     name = 'newlightparse'
     allowed_domains = ['https://www.divan.ru/ekaterinburg/']
     start_urls = ['https://www.divan.ru/ekaterinburg/category/svet']
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
         pass
