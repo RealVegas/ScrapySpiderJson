b5c3b2c82220618171c9143147d77ba53e2651de
Вадим Голов <grafit-design@inbox.ru>
2024-10-27 01:23:19 +0500

    Программа готова

--- lightparse/lightparse/spiders/newlightparse.py
+++ lightparse/lightparse/spiders/newlightparse.py
@@ -1,10 +1,32 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
 
 class NewlightparseSpider(scrapy.Spider):
+
     name = 'newlightparse'
-    allowed_domains = ['https://www.divan.ru/ekaterinburg/']
+    allowed_domains = ['https://www.divan.ru']
     start_urls = ['https://www.divan.ru/ekaterinburg/category/svet']
 
     def parse(self, response, **kwargs):
-        pass
+        lumins = response.css('div._Ud0k')
+
+        for lum in lumins:
+            yield {
+                'name': lum.css('div.lsooF span::text').get(),
+                'price': lum.css('div.pY3d2 span::text').get(),
+                'url': 'https://www.divan.ru' + lum.css('a::attr(href)').get()
+            }
+
+
+# Создание экземпляра CrawlerProcess с нужными настройками
+process = CrawlerProcess(settings=
+    {
+        'FEED_FORMAT': 'json',  # Формат вывода
+        'FEED_URI': 'luminaires.json',  # Имя файла для вывода
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    })
+
+# Запуск паука
+process.crawl(NewlightparseSpider)
+process.start()
